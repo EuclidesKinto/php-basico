779cf15184185a0d9905eab55ce79bb64d8d9375
Eulides Kinto <euclideskinto@gmail.com>
2020-11-27 14:59:51 -0300

    02_operadores_condicionais.php

---
+++ secao-01/02_operadores_condicionais.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Operadores Condicionais</title>
+</head>
+<body>
+<?php
+    //  ((V  e V) ou F) = V
+    if((22 == 22 && 3 == 3) || 5 != 5){
+        echo "Verdadeiro";
+    }else{
+        echo "Falso";
+    }
+
+?>
+
+</body>
+</html>
