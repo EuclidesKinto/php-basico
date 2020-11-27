292a79ab3522d01888b13853c67b97435edbcf0a
Eulides Kinto <euclideskinto@gmail.com>
2020-11-27 15:19:56 -0300

    update 02_operadores_condicionais.php. Condições encadeadas

--- secao-01/02_operadores_condicionais.php
+++ secao-01/02_operadores_condicionais.php
@@ -9,14 +9,60 @@
 </head>
 <body>
 <?php
-    //  ((V  e V) ou F) = V
-    if((22 == 22 && 3 == 3) || 5 != 5){
-        echo "Verdadeiro";
+//      ((V  e V) ou F) = V
+//    if((22 == 22 && 3 == 3) || 5 != 5){
+//        echo "Verdadeiro";
+//    }else{
+//        echo "Falso";
+//    }
+
+    //---------------------------------------------
+
+    $usuario_possui_cartao = true;
+    $valor_compra = 400;
+    $frete = 50;
+    $desconto_frete = true;
+
+    if($usuario_possui_cartao == true && $valor_compra >= 400){
+        $frete = 0;
+    }else if($usuario_possui_cartao == true && $valor_compra >= 300){
+        $frete = 10;
+    }else if($usuario_possui_cartao == true && $valor_compra >= 200){
+        $frete = 20;
+    }else if($usuario_possui_cartao == true && $valor_compra >= 100){
+        $frete = 30;
     }else{
-        echo "Falso";
+        $desconto_frete = false;
     }
 
+    $total = $valor_compra + $frete;
 ?>
+
+<h1>Detalhes do Pedido</h1>
+
+<p>Possui o cartão da loja? -
+    <?php
+        if($usuario_possui_cartao){
+            echo 'SIM';
+        }else{
+            echo 'Não';
+        }
+
+    ?>
+</p>
+<p>Valor da compra: R$<?= $valor_compra;?></p>
+<p>Recebeu desconto de frete? -
+    <?php
+    if($desconto_frete){
+        echo 'SIM';
+    }else{
+        echo 'Não';
+    }
+
+    ?>
+</p>
+<p>Valor do frete: R$<?= $frete;?></p>
+<p>Valor Total: R$<?= $total;?></p>
 
 </body>
 </html>
